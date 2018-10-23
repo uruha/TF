96d92bc60d1b8ed12e22b12111d24e27ae3dbbd3
uruha <kochmusica@gmail.com>
2018-10-23 22:30:19 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+
+const server = browserSync.create();
+
+function reload(done) {
+    server.reload();
+    done();
+}
+
+function serve(done) {
+    server.init({
+        server: {
+            baseDir: './',
+            open: 'local'
+        }
+    });
+    done();
+}
+
+function watch() {
+    gulp.watch('./*.html', gulp.series(reload));
+    gulp.watch('./module/*.js', gulp.series(reload));
+}
+
+gulp.task('default', gulp.series(serve, watch));
